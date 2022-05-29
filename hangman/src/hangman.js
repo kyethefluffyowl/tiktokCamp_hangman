bc74f32e0eed347904375de6ea28906b2f5b4d45
kyethefluffyowl <friendsbacktoback@gmail.com>
2022-05-29 22:25:21 +0800

    💐Aesthetics

--- hangman/src/hangman.js
+++ hangman/src/hangman.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { randomWord, randomWordWeb } from "./RandomWord.js";
 import "./hangman.css";
-import image0 from "./images/base.png";
-import image1 from "./images/1.png";
-import image2 from "./images/2.png";
-import image3 from "./images/3.png";
-import image4 from "./images/4.png";
-import image5 from "./images/5.png";
-import image6 from "./images/6.png";
+import image0 from "./images/base.svg";
+import image1 from "./images/1.svg";
+import image2 from "./images/2.svg";
+import image3 from "./images/3.svg";
+import image4 from "./images/4.svg";
+import image5 from "./images/5.svg";
+import image6 from "./images/6.svg";
+const confetti = require('canvas-confetti');
 
 class Hangman extends Component {
   static defaultProps = {
@@ -46,11 +47,12 @@ class Hangman extends Component {
       guessed: st.guessed.add(letter),
       noOfWrong: st.noOfWrong + (st.answer.includes(letter) ? 0 : 1),
     }));
-    if (letter === 'z') {
-        console.log("You clicked the letter Z");
-        console.log("The word in hangman.js is: " + randomWordWeb());
-        this.setState({noOfWrong: 999})
-    }
+
+    // if (letter === 'z') {
+    //     console.log("You clicked the letter Z");
+    //     console.log("The word in hangman.js is: " + randomWordWeb());
+    //     this.setState({noOfWrong: 999})
+    // }
   }
 
   generateKeypad() {
@@ -68,20 +70,68 @@ class Hangman extends Component {
       ));
   }
 
+  generateLives() {
+    
+      let content = [];
+      for (let i = 0; i<this.props.maxWrong - this.state.noOfWrong; i++) {
+          content.push("🤍");
+      }
+      return content;
+    }
+
+    generateConfetti() {
+    var myCanvas = document.createElement('canvas');
+    document.body.appendChild(myCanvas);
+    myCanvas.setAttribute('class', 'confettiCanvas');
+    document.getElementsByClassName("confettiCanvas")[0].removeAttribute("height");
+    
+    var myConfetti = confetti.create(myCanvas, {
+      resize: true,
+      useWorker: true
+    });
+
+    
+
+// confetti function YAY
+setTimeout(10);
+    myConfetti({
+      particleCount: 50,
+      angle:60,
+      spread: 55,
+      origin: {x:0}
+    });
+
+    myConfetti({
+      particleCount: 50,
+      angle:120,
+      spread: 55,
+      origin: {x:1}
+    });
+
+    myConfetti({
+        particleCount: 100,
+        startVelocity: 30,
+        spread: 360,
+        origin: {x: Math.random(), y: Math.random() - 0.2}
+      });
+
+    }
+
+
   render() {
     const gameOver = this.state.noOfWrong >= this.props.maxWrong;
     const isWinner = this.guessedWord().join("") === this.state.answer;
     let gameState = this.generateKeypad();
     if (isWinner) gameState = "WOW! You sure do have a way with words. :) Again?";
-    if (gameOver) gameState = "Better Luck Next Time";
+    if (isWinner) {this.generateConfetti();}
+    if (gameOver) gameState = "It's okay! You can try again! :)";
     let restart = gameOver || isWinner;
+    let livesLeft = this.generateLives();
     return (
       <div className="Hangman">
         <h2>Hangman</h2> {/* Title of the page */}
         <img src={this.props.images[this.state.noOfWrong]} alt="HangMan" id="hangman-image"/>
-        <p>
-          Guesses Remaining: {this.props.maxWrong - this.state.noOfWrong} /{" "}{this.props.maxWrong}
-        </p>
+        <p id="guessRemaining">{livesLeft}</p>
         <p className="Hangman-word">
           {!gameOver ? this.guessedWord() : this.state.answer}
         </p>
